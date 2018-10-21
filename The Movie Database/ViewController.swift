7e8aa888f8cfd7a6bc5e7337f298e8312714a123
Hariel Giacomuzzi <hariel.dias@acad.pucrs.br>
2018-10-21 09:53:32 -0300

    Initial Commit

---
+++ The Movie Database/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  The Movie Database
+//
+//  Created by Hariel Giacomuzzi on 21/10/18.
+//  Copyright © 2018 Giacomuzzi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
